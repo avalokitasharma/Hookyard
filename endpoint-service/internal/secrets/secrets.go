8f77d4d26f071f2f9ec56d77addfffbcb161c97a
avalokitasharma <avalokitasharma@gmail.com>
2026-09-15 19:43:02 +0530

    aes encryption

---
+++ endpoint-service/internal/secrets/secrets.go
@@ -0,0 +1,39 @@
+package secrets
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
+	"io"
+)
+
+type Cipher struct{ gcm cipher.AEAD }
+
+func NewFromHex(keyHex string) (*Cipher, error) {
+	key, err := hex.DecodeString(keyHex)
+	if err != nil {
+		return nil, fmt.Errorf("decode encryption key: %w", err)
+	}
+	if len(key) != 32 {
+		return nil, fmt.Errorf("encryption key must be 32 bytes")
+	}
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, fmt.Errorf("create cipher: %w", err)
+	}
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, fmt.Errorf("create gcm: %w", err)
+	}
+	return &Cipher{gcm: gcm}, nil
+}
+
+func (c *Cipher) Encrypt(plaintext string) ([]byte, error) {
+	nonce := make([]byte, c.gcm.NonceSize())
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		return nil, fmt.Errorf("generate nonce: %w", err)
+	}
+	return c.gcm.Seal(nonce, nonce, []byte(plaintext), nil), nil
+}
